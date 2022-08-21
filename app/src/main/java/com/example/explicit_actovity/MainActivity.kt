7ed4563d3b47c6bfe57e8b50639e777680392d8c
SAYAN <sayanhumanist@gmail.com>
2022-08-22 04:17:12 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/explicit_actovity/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.explicit_actovity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener{
+
+            val intent = Intent(this@MainActivity,Second_Activity::class.java)
+            startActivity(intent)
+
+
+
+
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/example/explicit_actovity/Second_Activity.kt
@@ -0,0 +1,9 @@
+package com.example.explicit_actovity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class Second_Activity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.second_activity) }}
